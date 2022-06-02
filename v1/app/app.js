e2ee9f69140f3d419cbb8c99cf4ae632dc3d4730
Chris <chris.d.fournier@gmail.com>
2022-06-02 16:52:32 -0400

    Continuing express and ejs setup

--- v1/app/app.js
+++ v1/app/app.js
@@ -5,13 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var songRouter = require('./routes/song');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
---
+++ v1/app/public/javascripts/scripts.js
@@ -0,0 +1,27 @@
+// CHANGE CSS WHEN PAGE LOADS
+$(document).ready(function(){
+  $("body").addClass("visible");
+  $("body").removeClass("hidden");
+});
+
+// COLOR THEME MODES
+window.addEventListener('DOMContentLoaded', function(theme) {
+  if(localStorage.getItem('theme') === 'true') {
+    $('html').toggleClass('dark');
+    $('input#switch-theme').prop('checked',true);
+  }
+  else {
+    localStorage.setItem('theme',false);
+  }
+});
+$('input#switch-theme').click(function() {
+  $('html').toggleClass('dark');
+  if(localStorage.getItem('theme') === 'true') {
+    $('input#switch-theme').prop('checked',false);
+    localStorage.setItem('theme',false);
+  }
+  else {
+    $('input#switch-theme').prop('checked',true);
+    localStorage.setItem('theme',true);
+  }
+});
