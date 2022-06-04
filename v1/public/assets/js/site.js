1dfa9b4e7c93f95f6e64cac16f1613aa18fde99d
Chris <chris.d.fournier@gmail.com>
2022-06-04 10:51:12 -0400

    Added alert component
    To display things like capo notes; also moving some script functions (color mode and alerts) to external js file.

--- v1/public/assets/js/site.js
+++ v1/public/assets/js/site.js
@@ -1,3 +1,4 @@
+// COLOR MODE CONTROLS
 window.addEventListener('DOMContentLoaded', function(theme) {
   if(localStorage.getItem('theme') === 'true') {
     $('html').toggleClass('dark');
@@ -6,15 +7,25 @@ window.addEventListener('DOMContentLoaded', function(theme) {
   else {
     localStorage.setItem('theme',false);
   }
-});
-$('input#switch-theme').click(function() {
-  $('html').toggleClass('dark');
-  if(localStorage.getItem('theme') === 'true') {
-    $('input#switch-theme').prop('checked',false);
-    localStorage.setItem('theme',false);
+  });
+  $('input#switch-theme').click(function() {
+    $('html').toggleClass('dark');
+    if(localStorage.getItem('theme') === 'true') {
+      $('input#switch-theme').prop('checked',false);
+      localStorage.setItem('theme',false);
+    }
+    else {
+      $('input#switch-theme').prop('checked',true);
+      localStorage.setItem('theme',true);
   }
-  else {
-    $('input#switch-theme').prop('checked',true);
-    localStorage.setItem('theme',true);
-  }
 });
+
+// ALERTS
+window.addEventListener('DOMContentLoaded', function(alerts) {
+  $('.alert').addClass('show');
+  setTimeout(function() {
+    $('.alert').removeClass('show') }, 10000);
+  });
+  $('.alert .message button.close').click( function() {
+    $('.alert').removeClass('show');
+  });
